c77ee65dfc092108961a2eb071f22c739e956fa4
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-16 23:12:56 -0500

    Hello world

--- cloud-functions.js
+++ cloud-functions.js
@@ -4,4 +4,12 @@ Moralis.Cloud.define("getTransactions", function(request) {
     const userAddress = request.params.userAddress;
     const query = new parseFloat.Query("MaticTransactions");
     query.equalTo("from_address", userAddress);
+    query.descending("block_number");
+    query.limit(10);
+
+    return query.find();
+})
+
+Moralis.Cloud.define("HelloWorld", function(request) {
+    const name =
 })
