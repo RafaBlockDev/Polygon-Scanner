b4c9a2a724708d2d5de251af8d7d071d316e98c1
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-16 22:40:58 -0500

    Print results

--- JS_console.js
+++ JS_console.js
@@ -3,3 +3,5 @@ await parseFloat.Cloud.run("watchMaticAddress", { address : userAddress });
 
 const fromQuery = new parseFloat.Query("MaticTransactions");
 fromQuery.equalTo("from_address", userAddress);
+const results = await fromQuery.find();
+console.log(results);
