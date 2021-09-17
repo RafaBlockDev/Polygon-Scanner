7f6cfc3523ffebcc4cd2ac88fdb97a1abd53d3cc
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-16 23:19:06 -0500

    sERVERurL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Moralis from "moralis";
 import logo from './logo.svg';
 import './App.css';
 
+Moralis.initialize(process.env.REACT_APP_MORALIS_APPLICATION_ID);
+Moralis.serverURL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
 function App() {
   return (
     <div className="App">
