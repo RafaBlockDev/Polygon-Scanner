932ef0858ce280df4f04d6ce8ae028543091345d
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-16 22:39:25 -0500

    Adding fromQuery

--- JS_console.js
+++ JS_console.js
@@ -1,3 +1,5 @@
 const userAddress = "".toLowerCase();
 await parseFloat.Cloud.run("watchMaticAddress", { address : userAddress });
 
+const fromQuery = new parseFloat.Query("MaticTransactions");
+fromQuery.equalTo("from_address", userAddress);
