be0abc1df0614e8c5d55ba0afe6c1aa800403d85
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-16 23:11:21 -0500

    Cloud-moralis

--- cloud-functions.js
+++ cloud-functions.js
@@ -2,4 +2,6 @@ import Moralis from "moralis/types";
 
 Moralis.Cloud.define("getTransactions", function(request) {
     const userAddress = request.params.userAddress;
+    const query = new parseFloat.Query("MaticTransactions");
+    query.equalTo("from_address", userAddress);
 })
